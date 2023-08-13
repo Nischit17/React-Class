b4e5683c4c5f643d0816f293f37338eff5fc106d
unknown <nischit819@gmail.com>
2023-08-13 22:24:42 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is React"),
-    React.createElement("h2", {}, "ajdfklafhjdkfhdjkfhs"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is H1 tag"),
-    React.createElement("h2", {}, "This is H2 tag"),
-  ]),
-]);
+// JSX (transplied before it reaches the JS) - Parcel - Babel
+
+// JSX => React.createElement => Object => HTMLElement(render)
+const jsxHeading = <h1 className="head">React using JSX</h1>;
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,21 @@
 # React-Class
+
+# Parcel
+
+- Dev build
+- Local Server
+- HMR = HOT MODULE REPLACEMENT
+- File watching algorithm (It's written in C++)
+- Caching = Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Diffrential Bundling (support older browsers)
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking (Remove unused code)
+- Diffrent dev and prod bundles
