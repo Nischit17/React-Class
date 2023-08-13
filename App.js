ae1457b65c2e596453a376c04ee1b4241e194be4
unknown <nischit819@gmail.com>
2023-08-13 16:39:15 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is H1 tag"),
-    React.createElement("h2", {}, "This is H2 tag"),
+    React.createElement("h1", {}, "This is React"),
+    React.createElement("h2", {}, "ajdfklafhjdkfhdjkfhs"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is H1 tag"),
