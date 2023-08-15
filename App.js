67bcc3aff9506253f9d45db93f0cb573ae12ebe4
unknown <nischit819@gmail.com>
2023-08-15 13:47:10 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX (transplied before it reaches the JS) - Parcel - Babel
-
-// JSX => React.createElement => Object => HTMLElement(render)
-const jsxHeading = (
+// React Element
+const heading = (
   <h1 className="head" tabIndex="5">
     React using JSX
   </h1>
 );
 
-console.log(jsxHeading);
+// React Functional Component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h1 className="heading">React Functional Component</h1>;
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
+console.log(<HeadingComponent />);
+root.render(<HeadingComponent />);
