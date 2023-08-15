c09f8f28d349178b25ce5463856eeab30ebbc4dd
unknown <nischit819@gmail.com>
2023-08-15 20:20:52 +0530

    episode-04

--- App.js
+++ App.js
@@ -1,22 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = (
-  <h1 className="head" tabIndex="5">
-    React using JSX
-  </h1>
-);
-
-// React Functional Component
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      <h1 className="heading">React Functional Component</h1>;
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>CONTACT US</li>
+          <li>CART</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320/jenbl572o0hsr8xfxfbs"
+      />
+      <h3>Spice Waves</h3>
+      <h4>Biriyani, North Indian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">SEARCH</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(<HeadingComponent />);
-root.render(<HeadingComponent />);
+
+root.render(<AppLayout />);
