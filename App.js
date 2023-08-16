ff0b7ccddd6f89e7d37dc63fa859c76c293e15cd
unknown <nischit819@gmail.com>
2023-08-16 22:27:52 +0530

    episode-04

--- App.js
+++ App.js
@@ -1823,31 +1823,16 @@ const resList = [
   },
 ];
 
+// not using keys (not acceptable) <<< index as key >>> unique id (best practice)
+
 const Body = () => {
   return (
     <div className="body">
       <div className="search">SEARCH</div>
       <div className="res-container">
-        <RestaurantCard resData={resList[0]} />
-        <RestaurantCard resData={resList[1]} />
-        <RestaurantCard resData={resList[2]} />
-        <RestaurantCard resData={resList[3]} />
-        <RestaurantCard resData={resList[4]} />
-        <RestaurantCard resData={resList[5]} />
-        <RestaurantCard resData={resList[6]} />
-        <RestaurantCard resData={resList[7]} />
-        <RestaurantCard resData={resList[8]} />
-        <RestaurantCard resData={resList[9]} />
-        <RestaurantCard resData={resList[10]} />
-        <RestaurantCard resData={resList[11]} />
-        <RestaurantCard resData={resList[12]} />
-        <RestaurantCard resData={resList[13]} />
-        <RestaurantCard resData={resList[14]} />
-        <RestaurantCard resData={resList[15]} />
-        <RestaurantCard resData={resList[16]} />
-        <RestaurantCard resData={resList[17]} />
-        <RestaurantCard resData={resList[18]} />
-        <RestaurantCard resData={resList[19]} />
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
