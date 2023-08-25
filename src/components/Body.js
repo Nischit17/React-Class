d4be477ae1f0ed380dd29a634da746a48c590535
unknown <nischit819@gmail.com>
2023-08-25 17:34:22 +0530

    episode-05

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,47 +4,7 @@ import { useState } from "react";
 
 const Body = () => {
   // Local State variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestaurant] = useState([
-    {
-      info: {
-        id: "10576",
-        name: "Pizza Hut",
-        cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 3.8,
-        sla: {
-          deliveryTime: 21,
-        },
-      },
-    },
-    {
-      info: {
-        id: "10575",
-        name: "KFC",
-        cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 4.1,
-        sla: {
-          deliveryTime: 21,
-        },
-      },
-    },
-    {
-      info: {
-        id: "10577",
-        name: "Dominos",
-        cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 4.5,
-        sla: {
-          deliveryTime: 21,
-        },
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
 
   return (
     <div className="body">
