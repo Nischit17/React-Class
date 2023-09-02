c7d5db9a088f86e8d4c3cd012cf84e0cf29dc2c7
unknown <nischit819@gmail.com>
2023-09-02 15:07:27 +0530

    episode-06-part-06(Search-Btn)

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  // Local State variable - Super powerful variable
   const [listOfRestaurants, setListOfRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  // Function LOGIC
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
@@ -21,16 +21,36 @@ const Body = () => {
     console.log(json);
     setListOfRestaurant(
       // Optional Chaining
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  // Ternary Operator
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurants = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setListOfRestaurant(filteredRestaurants);
+            }}
+          >
+            SEARCH
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
