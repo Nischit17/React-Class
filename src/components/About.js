506611ee45949349906569799ac855091f5b0ebf
unknown <nischit819@gmail.com>
2023-09-07 19:14:59 +0530

    episode-08-part-03(update-state-variables)

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ const About = () => {
     <div>
       <h1>ABOUT</h1>
       <h2>This is About page</h2>
-      <User name={"Nischit (Function)  "} />
       <UserClass name={"Nischit (Classs)"} location={"DAVANAGERE"} />
     </div>
   );
