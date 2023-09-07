97ada7f17862e5387fee072b1618fe41a40ecb49
unknown <nischit819@gmail.com>
2023-09-07 17:13:16 +0530

    episode-08-part-01(Class-Based-Component)

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>ABOUT</h1>
       <h2>This is About page</h2>
+      <User name={"Nischit (Function)  "} />
+      <UserClass name={"Nischit (Classs)"} location={"DAVANAGERE"} />
     </div>
   );
 };
